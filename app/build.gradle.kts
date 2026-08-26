903fe7a2337416369ba4624566bde9c686455fae
ashraful77 <116723692+ashraful77@users.noreply.github.com>
2026-08-26 12:27:42 +0530

    Update compile options for Java and Kotlin
    Set Java and Kotlin compatibility to version 17.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,14 @@ plugins {
 android {
     namespace = "com.themathguild.mytuitionmanager"
     compileSdk = 35
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlinOptions {
+    jvmTarget = "17"
+}
     defaultConfig {
         applicationId = "com.themathguild.mytuitionmanager"
         minSdk = 26
