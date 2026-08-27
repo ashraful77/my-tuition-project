3b523c8fc1babbc6a8fd7ff9ef78f95bda03fc00
ashraful77 <116723692+ashraful77@users.noreply.github.com>
2026-08-28 03:26:38 +0530

    Update build.gradle.kts for Android project configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,88 @@
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.themathguild.mytuitionmanager"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.themathguild.mytuitionmanager"
+        minSdk = 26
+        targetSdk = 35
+
+        versionCode = 3
+        versionName = "1.2"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    signingConfigs {
+        create("permanent") {
+            val keystorePath =
+                System.getenv("KEYSTORE_FILE")
+
+            if (!keystorePath.isNullOrBlank()) {
+                storeFile = file(keystorePath)
+                storePassword =
+                    System.getenv("KEYSTORE_PASSWORD")
+                keyAlias =
+                    System.getenv("KEY_ALIAS")
+                keyPassword =
+                    System.getenv("KEY_PASSWORD")
+            }
+        }
+    }
+
+    buildTypes {
+        debug {
+            signingConfig =
+                signingConfigs.getByName("permanent")
+        }
+    }
+}
+
+dependencies {
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2024.12.01"
+        )
+    )
+
+    implementation(
+        "androidx.activity:activity-compose:1.10.0"
+    )
+
+    implementation(
+        "androidx.core:core-ktx:1.15.0"
+    )
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
+
+    implementation(
+        "androidx.compose.ui:ui"
+    )
+
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
 }
