60a889fdb7626230b7c9ca691b1c363d9b4d4048
ashraful77 <116723692+ashraful77@users.noreply.github.com>
2026-09-21 11:51:53 +0530

    Increase app version for update installation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ kotlinOptions {
         minSdk = 26
         targetSdk = 35
     buildFeatures { compose = true }
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 }
 dependencies {
